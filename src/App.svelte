1e2079417ed8e335f933f797484d24ece53add69
HADMARINE <contact@hadmarine.com>
2020-03-06 14:29:49 +0900

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Router, Link, Route } from "svelte-routing";
+  import PageNotFound from "./pages/PageNotFound.svelte";
+
+  export let url = "";
+</script>
+
+<style>
+  :global(body) {
+    margin: 0;
+    padding: 0;
+  }
+</style>
+
+<Router {url}>
+  <div>
+    <Route component={PageNotFound} />
+  </div>
+</Router>
---
+++ src/pages/PageNotFound.svelte
@@ -0,0 +1,14 @@
+<style>
+  .wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+    font-size: 40px;
+    background-color: black;
+    color: white;
+  }
+</style>
+
+<div class="wrapper">Page Not Found</div>
